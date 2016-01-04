4824784eab580080204e436b3d39b1f27456b2d3
patrickdnr <patrickdn.robertson96@gmail.com>
2016-01-04 23:24:13 +0000

    Added campgrounds page tested display functionality with hard-coded data

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # yelp-camp
+
+05/01/15
+- Express and ejs installed via npm
+- Initial routes and listerner added
--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Routes
 app.get("/", function(req, res) {
    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res) {
+    var campgrounds = [
+            {name: "Salmon Creek", image: "http://photosforclass.com/download/3844623716"},
+            {name: "Granite Hill", image: "http://photosforclass.com/download/4344237662"},
+            {name: "Shoreham", image: "http://photosforclass.com/download/15623542806"}
+        ];
+        
+    res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 // Listener
